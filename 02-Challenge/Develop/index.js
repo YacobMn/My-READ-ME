7b96155f5bfaa19f51d4bd42a9d6f94b0f5c7bfa
unknown <yacobabr@gmail.com>
2023-11-02 20:20:47 -0500

    last-one

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,13 +1,32 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
-
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'what is your TITLE?',
+        name: 'title',
+    }
+]).then(file => {
+    console.log(file)
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+    }
+})
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+// generate the readMe file. 
+function init() {
+    //1 get the questions and the use the answer to =>
+    //2. wwrite the file.
+
+}
 
 // Function call to initialize app
 init();
